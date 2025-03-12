9e9a7fad91d46b16d0ddabf9a2739ae8ce8a7661
sid0417 <roysid1708@gmail.com>
2025-03-12 23:57:38 +0530

    Commit 2

--- src/app/Loading/page.tsx
+++ src/app/Loading/page.tsx
@@ -4,7 +4,7 @@ interface LoadingProps {
   value: string;
 }
 
-export default function Loading({ value }: LoadingProps) {
+export function Loading({ value }: LoadingProps) {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-b from-black to-[#1a1f38] text-white">
       <div className="animate-spin rounded-full h-12 w-12 border-t-4 border-blue-500"></div>
